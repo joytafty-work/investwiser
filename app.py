a6ba13c7803567175546a19a201aaf0fc3faf3e7
joytafty <joytafty@gmail.com>
2014-01-26 19:03:53 -0800

    utilize heroku port

--- app.py
+++ app.py
@@ -67,6 +67,10 @@ def regularpage(pagename=None):
        raise Exception, 'page_not_found' 
     return render_template(pagename) 
 
-if __name__ == '__main__':
-    print "Starting debugging server."
-    app.run(debug=True, host='localhost', port=8000)
+# if __name__ == '__main__':
+#     print "Starting debugging server."
+#     app.run(debug=True, host='localhost', port=8000)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
