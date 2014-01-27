2b6e7111f1643b0bc7ea91a0db3d7eb34f824cca
joytafty <joytafty@gmail.com>
2014-01-26 19:07:36 -0800

    update visualize.js

--- app.py
+++ app.py
@@ -72,5 +72,5 @@ def regularpage(pagename=None):
 #     app.run(debug=True, host='localhost', port=8000)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
